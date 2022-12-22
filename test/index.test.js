536e6a5c12c6e0c674b451d9e75b47eb7496a6f0
Leonard Menzel <dev@leomenzel.de>
2022-12-22 14:38:20 +0100

    add software test

---
+++ test/index.test.js
@@ -0,0 +1,39 @@
+import { now, since, span } from '../src'
+import 'mocha'
+import { assert } from 'chai'
+
+async function waitFor(waitForMs) {
+    return new Promise((arg) => setTimeout(arg, waitForMs));
+}
+
+describe('Test now()', () => {
+    const start = now()
+    it('should a value greater zero', () => {
+        assert.isAbove(Number(start), 0)
+    })
+})
+
+describe('Test since()', () => {
+    const start = now()
+    it('should return a value greater zero', async() => {
+        waitFor(2)
+        assert.isAbove(since(start), 0)
+    })
+    it('should return correct milliseconds when waiting', async () => {
+        const q = now()
+        const waitForMs = 10
+        const p = performance.now()
+        await waitFor(waitForMs)
+        assert.equal(Math.round(since(q)), Math.round((performance.now() - p)))
+    })
+})
+
+describe('Test span()', () => {
+    it('shoud return a value greater zero', async () => {
+        const start = now()
+        await waitFor(2)
+        const until = now()
+        await waitFor(2)
+        assert.isAbove(span(start, until), 0)
+    })
+})
